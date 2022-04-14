b0562b649d553bd04a54b2fbf30aea816fabcdb5
Florence D. Neige <101849240+snowflakeuniversity@users.noreply.github.com>
2022-04-13 20:03:40 -0700

    Update index.py

--- index.py
+++ index.py
@@ -1,10 +1,9 @@
 import streamlit as st
 import snowflake.connector 
 
-from PIL import Image
-image = Image.open('sunrise.jpg')
+IMAGE_URL = "https://images.unsplash.com/photo-1548407260-da850faa41e3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1487&q=80"
 
-st.image(image, caption='Sunrise by the mountains')
+st.image(IMAGE_URL, caption="Sunrise by the mountains")
 
 st.title('You are cordially invited to...')
 st.header('The Private Snowflake Exchange ACME-ADU')
