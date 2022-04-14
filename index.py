483af65a79fd1fdab85471ef9f5338e6bc572d71
Florence D. Neige <101849240+snowflakeuniversity@users.noreply.github.com>
2022-04-13 20:24:51 -0700

    Update index.py

--- index.py
+++ index.py
@@ -5,19 +5,14 @@ import snowflake.connector
 
 st.title('You are cordially invited to...')
 st.header('The Private Snowflake Exchange ACME-ADU')
-
-
-st.write('Did you know Private Exchanges only exist on one Region/Cloud?') 
-st.write('Our ACME-ADU Exchange only exists in the AWS Central Canada Region.') 
-st.write('We can only add your account if it is in the same region as the exchange.')
-  
+ 
 current_region_function = "https://learn.snowflake.com/asset-v1:snowflake+ESS-SMEW+C+type@asset+block@current_region_function.png"
 st.image(current_region_function)
 
 option = st.selectbox(
      'What REGION do you see if you run the CURRENT_REGION function?',
-     ('<pick one>','AWS_CA_CENTRAL_1', 'Something that is not AWS and Canada Central')) 
-st.write('You selected:', option)
+     ('<pick one>','AWS_CA_CENTRAL_1', 'Something other region.'))
+
 
 current_account_function = "https://learn.snowflake.com/asset-v1:snowflake+ESS-SMEW+C+type@asset+block@current_account_function.png"
 st.image(current_account_function)
