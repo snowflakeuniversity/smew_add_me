894ca98eacb4e29cff9c7a1d492d639a674e1638
Florence D. Neige <101849240+snowflakeuniversity@users.noreply.github.com>
2022-04-13 20:23:16 -0700

    Update index.py

--- index.py
+++ index.py
@@ -1,9 +1,7 @@
 import streamlit as st
 import snowflake.connector 
 
-current_account_function = "https://learn.snowflake.com/asset-v1:snowflake+ESS-SMEW+C+type@asset+block@current_account_function.png"
 
-st.image(current_account_function)
 
 st.title('You are cordially invited to...')
 st.header('The Private Snowflake Exchange ACME-ADU')
@@ -13,12 +11,17 @@ st.write('Did you know Private Exchanges only exist on one Region/Cloud?')
 st.write('Our ACME-ADU Exchange only exists in the AWS Central Canada Region.') 
 st.write('We can only add your account if it is in the same region as the exchange.')
   
+current_region_function = "https://learn.snowflake.com/asset-v1:snowflake+ESS-SMEW+C+type@asset+block@current_region_function.png"
+st.image(current_region_function)
+
 option = st.selectbox(
-     'Run the command [SELECT CURRENT_REGION();] What is the result?',
+     'What REGION do you see if you run the CURRENT_REGION function?',
      ('<pick one>','AWS_CA_CENTRAL_1', 'Something that is not AWS and Canada Central')) 
 st.write('You selected:', option)
 
-title = st.text_input('Your Account Locator', 'abc12345')
+current_account_function = "https://learn.snowflake.com/asset-v1:snowflake+ESS-SMEW+C+type@asset+block@current_account_function.png"
+st.image(current_account_function)
+title = st.text_input('What is listed if your run the CURRENT ACCOUNT function?', 'abc12345')
 st.write('Your Account Locator is ', title)
 
 my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
