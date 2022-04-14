aeb0cba618fcc76bf1c45c5d27eb8168c8ce50fa
Florence D. Neige <101849240+snowflakeuniversity@users.noreply.github.com>
2022-04-13 19:36:33 -0700

    Update index.py

--- index.py
+++ index.py
@@ -1,3 +1,10 @@
 import streamlit
 
 streamlit.title('You are cordially invited to the Private Snowflake Exchange ACME-ADU')
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
